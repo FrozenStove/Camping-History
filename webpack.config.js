5dd20b8e9f14d83532d539ae1f9ae413a46f84c9
FrozenStove <Yessysutter@gmail.com>
2022-10-14 19:02:09 -0700

    Webpack Progress

--- webpack.config.js
+++ webpack.config.js
@@ -6,60 +6,63 @@ const ArcGISPlugin = require("@arcgis/webpack-plugin");
 
 //https://stackoverflow.com/questions/61767538/devtools-failed-to-load-sourcemap-for-webpack-node-modules-js-map-http-e
 module.exports = {
-    entry: './client/index.js',
-    output: {
-        path: path.resolve(__dirname, './build'),
-        filename: 'bundle.js',
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, './build'),
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  plugins: [new HtmlWebpackPlugin({
+    filename: 'bundle.html',
+    template: 'index.html'
+  }),
+  new SourceMapDevToolPlugin({
+    filename: "[file].map"
+  }),
+  new ArcGISPlugin({
+    // features: {
+    //     "3d": false
+    // },
+    locales: ['en'],
+  })
+  ],
+  devServer: {
+    static: {
+      publicPath: '/',
+      directory: path.resolve(__dirname)
     },
-    mode: 'development',
-    plugins: [new HtmlWebpackPlugin({        
-        filename: 'bundle.html',        
-        template: 'index.html'
-    }),
-    new SourceMapDevToolPlugin({
-        filename: "[file].map"
-    }),
-    new ArcGISPlugin({
-        // features: {
-        //     "3d": false
-        // },
-        locales: ['en'],
-    })
-    ], 
-    devServer: {
-        static: {
-          publicPath: '/',
-          directory: path.resolve(__dirname)
+    // proxy: {
+    //   '/*': 'http://localhost:3000/',
+    // },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            "@babel/preset-react",
+            "@babel/preset-env"
+          ],
         }
-    },
-    module: {    
-        rules: [
-            {
-                test: /\.jsx?/,
-                exclude: /node_modules/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        "@babel/preset-react", 
-                        "@babel/preset-env"
-                    ],
-                }
-            },
-            {
-                test: /\.scss/,
-                use: [
-                    "style-loader", // create style tags from JS strings
-                    "css-loader", // CSS to CommonJS
-                    "sass-loader", // Sass to CSS
-                ],
-            },
-            {
-                test: /\.js$/,
-                enforce: 'pre',
-                use: ['source-map-loader'],
-              },
+      },
+      {
+        test: /\.scss/,
+        use: [
+          "style-loader", // create style tags from JS strings
+          "css-loader", // CSS to CommonJS
+          "sass-loader", // Sass to CSS
         ],
-    },
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
+    ],
+  },
 };
 
 
