89315f1e600326472bad2ebdcde3013908df3a7f
FrozenStove <Yessysutter@gmail.com>
2022-12-07 09:42:58 -0800

    style

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import HistoryCard from "./historyCard.jsx"
 import GisMap from "./GisMap.jsx";
 import LoginForm from "./login.jsx"
-import '../../style.css'
+import '../../style.scss'
 
 class App extends Component {
     constructor(props) {
